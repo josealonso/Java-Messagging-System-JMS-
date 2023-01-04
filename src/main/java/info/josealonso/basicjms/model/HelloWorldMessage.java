79fcdb3ea4926610f0bb4ddbe5866a362723fd0d
josealonso <jose.alonso.programmer@gmail.com>
2023-01-04 17:00:57 +0100

    Created model.

---
+++ src/main/java/info/josealonso/basicjms/model/HelloWorldMessage.java
@@ -0,0 +1,20 @@
+package info.josealonso.basicjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = 4805885535400307300L;
+    private UUID uuid;
+    private String message;
+}
