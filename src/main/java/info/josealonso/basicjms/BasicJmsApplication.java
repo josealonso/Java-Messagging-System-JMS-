502e86ce0d8e5a13dc8b089de8eecf8f436fce83
josealonso <jose.alonso.programmer@gmail.com>
2023-01-04 16:48:37 +0100

    Set up Spring Boot project to get started with JMS and an embedded activemq server.

---
+++ src/main/java/info/josealonso/basicjms/BasicJmsApplication.java
@@ -0,0 +1,13 @@
+package info.josealonso.basicjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/josealonso/basicjms/BasicJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package info.josealonso.basicjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
